cd347c8b1127e4bcee751e615aa1e58e1fbbb943
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-13 16:38:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,8 @@ def get_japanese_emoticon(file_path, emoticon)
   # code goes here
   library_hash = load_library(file_path)
 
-
+  result = load_library(file_path)['get_emoticon'][emoticon]
+  result ? result : "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning
